9eff93a7e09b36ede91e8eef01dd0f86201b85be
Bruno Lamps <lamparolho@gmail.com>
2021-09-03 15:11:29 -0300

    fix small login annoyances and add decorator

--- api/src/users/users.controller.ts
+++ api/src/users/users.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { GetUser } from 'src/auth/getUser.decorator';
+import JwtAuthGuard from 'src/auth/jwtAuth.guard';
+import { User } from './user.entity';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+
+    @Get('details')
+    @UseGuards(JwtAuthGuard)
+    async getDetails(@GetUser() usr: User) {
+        console.log(usr);
+        return 'user details';
+    }
+
+}
